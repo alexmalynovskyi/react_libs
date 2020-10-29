001e24d000f7bbe9c8007518a9c97d586a1679ae
ionelaunicorn <lupashkuionela@gmail.com>
2020-10-29 07:05:39 +0200

    init commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import { store } from './store/configureStore';
 
 import ItemList from './components/ItemList';
-
-const store = configureStore();
 
 render(
     <Provider store={store}>
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-
-export default function configureStore(initialState) {
-    return createStore(
-        rootReducer,
-        initialState,
-        applyMiddleware(thunk)
-    );
-}
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const persistConfig = {
+    key: 'root',
+    storage,
+    // whitelist: ['authType'] // which reducer want to store
+  }
+  const pReducer = persistReducer(persistConfig, rootReducer);
+  const middleware = applyMiddleware(thunk);
+  const store = createStore(pReducer, middleware);
+  const persistor = persistStore(store);
+  export { persistor, store };
